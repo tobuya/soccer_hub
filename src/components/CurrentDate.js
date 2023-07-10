45c9b4b540310308b9c6400d8071abc8d65e9539
tobuya <thomasobuya95@gmail.com>
2023-07-10 11:56:00 +0300

    Add a component that gets the current date

---
+++ src/components/CurrentDate.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+function CurrentDate() {
+  const [currentDate, setCurrentDate] = useState('');
+
+  useEffect(() => {
+    const getCurrentDate = () => {
+      const date = new Date();
+      const formattedDate = date.toISOString().slice(0, 10);
+      setCurrentDate(formattedDate);
+    };
+
+    getCurrentDate();
+  }, []);
+
+  return <span>{currentDate}</span>;
+}
+
+export default CurrentDate;
