4479c3b141265678b9327dfdbc7ff65ca2fddbf9
tobuya <thomasobuya95@gmail.com>
2023-07-11 15:18:24 +0300

    Add a competition title name in the competition matches

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { FiArrowRightCircle, FiArrowLeftCircle } from 'react-icons/fi';
 import { fetchData } from '../Redux/Competition/competition';
+import CurrentDate from './CurrentDate';
 
 const Competition = () => {
   const dispatch = useDispatch();
@@ -32,36 +33,47 @@ const Competition = () => {
 
   return (
     <>
-      <div className="input-group px-5 py-2">
-        <input
-          type="text"
-          className="form-control"
-          aria-label="Sizing example input"
-          aria-describedby="inputGroup-sizing"
-          value={searchCompetitionName}
-          onChange={(event) => setCompetitionSearchName(event.target.value)}
-          placeholder="Enter the competition name ..."
-        />
-      </div>
+      {!selectedCompetition && (
+        <div className="input-group px-5 py-2">
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing"
+            value={searchCompetitionName}
+            onChange={(event) => setCompetitionSearchName(event.target.value)}
+            placeholder="Enter the competition name ..."
+          />
+        </div>
+      )}
 
-      <div className="d-flex flex-wrap mx-4 mb-3">
+      <div className="d-flex flex-wrap mx-4 my-2">
         {selectedCompetition ? (
-          <div className="d-flex flex-lg-row flex-wrap w-100 m-2 col-lg-4 col-md-6 col-sm-12 flex-sm-column">
-            {matchesByCompetition[selectedCompetition].map((match) => (
-              <main key={matchesByCompetition[selectedCompetition].id} className="flex-grow-1 m-lg-2 m-sm-1 col-lg-5 col-md-12 border border-seconday shadow-lg rounded">
-                <FiArrowLeftCircle onClick={handleMatchClose} className="m-lg-2 m-sm-1 hover" />
-                <div key={match.matchId} className="d-flex flex-column mb-2 mx-lg-3 mx-sm-2">
-                  <div className="d-flex justify-content-between">
-                    <p className="fw-bolder match-title">{match.matchTitle}</p>
-                    <p className="fw-bolder match-date">{new Date(match.datePlayed).toDateString()}</p>
-                  </div>
-                  <div className="">
-                    <div dangerouslySetInnerHTML={{ __html: match.highlightsVideo }} className="highlight" />
+          <section className="d-flex flex-column w-100 gap-2">
+            <h4 className="text-center competition-name">
+              {selectedCompetition}
+              {' '}
+              (
+              <CurrentDate />
+              )
+            </h4>
+            <main className="d-flex flex-lg-row flex-column flex-wrap col-12">
+              {matchesByCompetition[selectedCompetition].map((match) => (
+                <div key={match.matchId} className="col-lg-6 col-md-12 border border-seconday shadow-lg rounded">
+                  <FiArrowLeftCircle onClick={handleMatchClose} className="m-lg-2 m-sm-1 hover" />
+                  <div key={match.matchId} className="d-flex flex-column mb-2 mx-lg-3 mx-sm-2">
+                    <div className="d-flex justify-content-between">
+                      <p className="fw-bolder match-title">{match.matchTitle}</p>
+                      <p className="fw-bolder match-date">{new Date(match.datePlayed).toDateString()}</p>
+                    </div>
+                    <div className="">
+                      <div dangerouslySetInnerHTML={{ __html: match.highlightsVideo }} className="highlight" />
+                    </div>
                   </div>
                 </div>
-              </main>
-            ))}
-          </div>
+              ))}
+            </main>
+          </section>
         ) : (
           Object.keys(matchesByCompetition)
             .filter(
