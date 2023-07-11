2c06f1e9e773388e6ebf3eca374c72fc4f929b29
tobuya <thomasobuya95@gmail.com>
2023-07-11 13:56:14 +0300

    Update homepage component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -25,50 +25,63 @@ const Competition = () => {
     setSelectedCompetition(null);
   };
 
-  const competitions = Object.keys(matchesByCompetition).map((competitionName) => ({
-    name: competitionName,
-    matchCount: matchesByCompetition[competitionName].length,
-  }));
-
-  competitions.sort((a, b) => b.matchCount - a.matchCount);
+  const getMatchCount = (competitionName) => {
+    const matches = matchesByCompetition[competitionName];
+    return matches ? matches.length : 0;
+  };
 
   return (
     <>
-      <div className="input-group mt-2 input-group-lg">
-        <span className="input-group-text" id="inputGroup-sizing-lg">Competition</span>
-        <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" value={searchCompetitionName} onChange={(event) => setCompetitionSearchName(event.target.value)} placeholder="Enter competition name ..." />
+      <div className="input-group px-5 py-2">
+        <input
+          type="text"
+          className="form-control"
+          aria-label="Sizing example input"
+          aria-describedby="inputGroup-sizing"
+          value={searchCompetitionName}
+          onChange={(event) => setCompetitionSearchName(event.target.value)}
+          placeholder="Enter the competition name ..."
+        />
       </div>
-      <div className="d-flex flex-fill flex-wrap m-3 border css-style background">
+
+      <div className="d-flex flex-wrap mx-4 mb-3">
         {selectedCompetition ? (
-          <div className="d-flex flex-column gap-3">
-            <FiArrowLeftCircle onClick={handleMatchClose} className="mb-4  hover" />
+          <div className="d-flex flex-lg-row flex-wrap w-100 m-2 col-lg-4 col-md-6 col-sm-12 flex-sm-column">
             {matchesByCompetition[selectedCompetition].map((match) => (
-              <div key={match.matchId} className="d-flex flex-column mb-4 flex-grow-1 css-style ms-5">
-                <div className="d-flex justify-content-between">
-                  <p className="fw-bolder">{match.matchTitle}</p>
-                  <p className="fw-bolder">{new Date(match.datePlayed).toDateString()}</p>
+              <main key={matchesByCompetition[selectedCompetition].id} className="flex-grow-1 m-lg-2 m-sm-1 col-lg-5 col-md-12 border border-seconday shadow-lg rounded">
+                <FiArrowLeftCircle onClick={handleMatchClose} className="m-lg-2 m-sm-1 hover" />
+                <div key={match.matchId} className="d-flex flex-column mb-2 mx-lg-3 mx-sm-2">
+                  <div className="d-flex justify-content-between">
+                    <p className="fw-bolder match-title">{match.matchTitle}</p>
+                    <p className="fw-bolder match-date">{new Date(match.datePlayed).toDateString()}</p>
+                  </div>
+                  <div className="">
+                    <div dangerouslySetInnerHTML={{ __html: match.highlightsVideo }} className="highlight" />
+                  </div>
                 </div>
-                <div className="d-flex justify-content-between">
-                  <img src={match.thumbnail} alt="Match" width="49.5%" className="highlight" />
-                  <div dangerouslySetInnerHTML={{ __html: match.highlightsVideo }} style={{ width: '49.5%' }} className="highlight" />
-                </div>
-              </div>
+              </main>
             ))}
           </div>
         ) : (
-          competitions.map(({ name, matchCount }) => (
-            <div key={name} className="d-flex align-items-center justify-content-center flex-grow-1">
-              <button type="button" className="d-flex flex-column gap-2 p-5 flex-fill align-items-center justify-content-center" onClick={() => handleCompetitionClick(name)}>
-                <FiArrowRightCircle className="order-1" />
-                {name}
-                <p>
-                  Matches in this competition:
-                  {' '}
-                  {matchCount}
-                </p>
-              </button>
-            </div>
-          ))
+          Object.keys(matchesByCompetition)
+            .filter(
+              (competitionName) => competitionName
+                .toLowerCase()
+                .includes(searchCompetitionName.toLowerCase()),
+            )
+            .map((competitionName) => (
+              <div key={competitionName} className="d-flex align-items-center justify-content-center flex-grow-1 col-lg-4 col-md-6">
+                <button type="button" className="d-flex flex-column gap-3 p-5 flex-fill align-items-center justify-content-center flex-1" onClick={() => handleCompetitionClick(competitionName)}>
+                  <FiArrowRightCircle className="order-1" />
+                  {competitionName}
+                  <p className="text-muted">
+                    Total Matches:
+                    {' '}
+                    {getMatchCount(competitionName)}
+                  </p>
+                </button>
+              </div>
+            ))
         )}
       </div>
     </>
